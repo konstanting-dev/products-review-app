6aae32d133b1fcbfe022fb8019d580b29fc61138
Kanstantsin Halavan <kanstantsin.h@gmail.com>
2021-07-05 22:36:45 +0300

    clean up api service

--- client/src/api/apiService.ts
+++ client/src/api/apiService.ts
@@ -6,28 +6,6 @@ const axiosApiInstance = axios.create({
   baseURL: `${process.env.REACT_APP_API_URL}`,
 });
 
-// request delay for mocked API testing purposes
-const REQUEST_DELAY = +(process.env.REACT_APP_REQUEST_DELAY || 0);
-
-// Request interceptor to set authorization token
-axiosApiInstance.interceptors.request.use(
-  async (config) => {
-    const token = localStorage.getItem('access_token') || 'test';
-    config.headers = {
-      Authorization: `Bearer ${token}`,
-    };
-
-    if (REQUEST_DELAY) {
-      return new Promise((resolve) => setTimeout(() => resolve(config), REQUEST_DELAY));
-    }
-
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
-);
-
 // Response interceptor to get error message and emit new 'responseError' event with it
 // We listen to 'responseError' event in Snackbar provider to show errors in the app
 axiosApiInstance.interceptors.response.use(
